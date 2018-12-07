32534e4fec9f72dd22af61ce4c36c494bde7fe92
18033281 <18033281@cnsuning.com>
2018-12-07 15:34:33 +0800

    d.ts修复

--- sqliteHelper.d.ts
+++ sqliteHelper.d.ts
@@ -1,43 +1,44 @@
-/// <reference name="SQLite" />
 interface Field {
-    columnName: string;
-    dataType: string;
+  columnName: string;
+  dataType: string;
 }
 
 interface Table {
-    tableName: string;
-    tableFields: Array<Field>;
+  tableName: string;
+  tableFields: Array<Field>;
 }
 
 interface SQLiteProps {
-    databaseName: string;
-    databaseVersion: string;
-    databaseDisplayName: string;
-    databaseSize: number;
+  databaseName: string;
+  databaseVersion: string;
+  databaseDisplayName: string;
+  databaseSize: number;
 }
 
 interface Result {
-    res: any;
-    err: Error;
+  res: any;
+  err: Error;
 }
 
 export default class SQLite {
-    static delete(database: string): Result;
+  static delete(database: string): Result;
 
-    constructor(props: SQLiteProps);
+  constructor(props: SQLiteProps);
 
-    successInfo: (name: string, absolutely?: boolean) => void;
-    errorInfo: (name: string, err: object, absolutely?: boolean) => void;
-    open: () => Result;
-    close: () => Result;
-    createTable: (tableInfo: Table) => Result;
-    dropTable: (tableName: string) => Result;
-    insertItems: (tableName: string, items: object[]) => Result;
-    deleteItem: (tableName: string, condition?: object) => Result;
-    updateItem: (tableName: string, item: object, condition?: object) => Result;
-    selectItems: (tableName: string,
-                  columns: string[] | '*',
-                  condition: object | null,
-                  pagination?: number,
-                  perPageNum?: number) => Result;
+  successInfo: (name: string, absolutely?: boolean) => void;
+  errorInfo: (name: string, err: object, absolutely?: boolean) => void;
+  open: () => Result;
+  close: () => Result;
+  createTable: (tableInfo: Table) => Result;
+  dropTable: (tableName: string) => Result;
+  insertItems: (tableName: string, items: object[]) => Result;
+  deleteItem: (tableName: string, condition?: object) => Result;
+  updateItem: (tableName: string, item: object, condition?: object) => Result;
+  selectItems: (
+    tableName: string,
+    columns: string[] | "*",
+    condition: object | null,
+    pagination?: number,
+    perPageNum?: number
+  ) => Result;
 }
