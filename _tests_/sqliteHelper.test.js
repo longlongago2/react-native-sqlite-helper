47287cdaeb349132d072ee261ee18986fc372c61
longlongago2 <1789136583@qq.com>
2019-02-28 20:57:38 +0800

    update test.js

--- _tests_/sqliteHelper.test.js
+++ _tests_/sqliteHelper.test.js
@@ -1,4 +1,26 @@
-import sqliteHelper from '../sqliteHelper';
+import SQLiteStorage from 'react-native-sqlite-storage';
+import SQLiteHelper from '../sqliteHelper';
 
-// 测试执行前操作
-beforeAll(() => { });
+__DEV__ = false;
+
+jest.mock('react-native-sqlite-storage'); // 模拟sqliteHelper.js中的依赖模块
+
+describe('Test instance method: open()', () => {
+    test('sqliteHelper open sqlite database success', () => {
+        const sqliteHelper = new SQLiteHelper('test.db', '1.0', 'users', 2000);
+        SQLiteStorage.openDatabase.mockResolvedValue('open success'); // 模拟open方法中使用到的依赖方法
+        sqliteHelper.open().then(({ res, err }) => {
+            expect(res).toEqual('open success');
+        });
+    });
+
+    test('sqliteHelper open sqlite database failed', () => {
+        const sqliteHelper = new SQLiteHelper('test.db', '1.0', 'users', 2000);
+        SQLiteStorage.openDatabase.mockRejectedValue(new Error('open failed'));
+        sqliteHelper.open().then(({ res, err }) => {
+            expect(err.message).toEqual('open failed');
+        });
+    });
+});
+
+// describe('Test static method: delete()', () => { });
